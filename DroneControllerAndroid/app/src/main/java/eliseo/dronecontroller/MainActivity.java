184f34593a9e6827ad161a5d8398f9914b491bb2
Eliseo Martelli <martely98@gmail.com>
2015-05-18 14:49:29 +0200

    Android Interface

---
+++ DroneControllerAndroid/app/src/main/java/eliseo/dronecontroller/MainActivity.java
@@ -0,0 +1,23 @@
+package eliseo.dronecontroller;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
